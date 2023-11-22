8ee674af7e1f6539b770cf2a1d0e89105a762421
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-11-22 10:19:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function toFahrenheit(celsius) {
  // Write your code here
+	let farenhite = (celsius * 9 / 5) + 32;
+	farenhite = farenhite.toFixed(2);
+	return farenhite;
 }
 
 // Do not change the code below
